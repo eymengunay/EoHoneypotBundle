01eb43919825ada30756b682402d8e152e8baa31
Eymen Gunay <eymen@egunay.com>
2013-09-26 23:56:34 +0200

    [Minor] Licensify

--- Entity/HoneypotPrey.php
+++ Entity/HoneypotPrey.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the EoHoneypotBundle package.
+ *
+ * (c) Eymen Gunay <eymen@egunay.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Eo\HoneypotBundle\Entity;
 
 use Eo\HoneypotBundle\Model\HoneypotPrey as BaseHoneypotPrey;
