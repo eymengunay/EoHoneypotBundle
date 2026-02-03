d22c40ad1d3bdc7a8d3f91871396764d8c9f81a8
Martin Güthler <mg@webfactory.de>
2026-02-03 15:25:20 +0100

    Add return type

--- DependencyInjection/EoHoneypotExtension.php
+++ DependencyInjection/EoHoneypotExtension.php
@@ -27,7 +27,7 @@ class EoHoneypotExtension extends Extension
     /**
      * {@inheritDoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
